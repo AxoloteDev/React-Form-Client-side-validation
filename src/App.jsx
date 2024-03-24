20f6f7aac1ac456f805e4624b7f3df7815b17fd6
AxoloteDev <axolote.dev@gmail.com>
2024-03-24 11:30:13 -0300

    The path of the images has been corrected. the document has been formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Text from './Text.jsx'
-import reactLogo from './images/img.webp';
+import Text from "./Text.jsx";
+import reactLogo from "/static/images/img.webp";
 
-import './styles/App.css'
+import "./styles/App.css";
 
-function Form(){
+function Form() {
   const [name, setName] = useState("");
   const [nameVerificarion, setNameVerification] = useState(false);
   const [nameError, setNameError] = useState("");
@@ -18,70 +18,100 @@ function Form(){
   const [isLoading, setIsLoading] = useState(false);
   const [isSend, setIsSend] = useState(false);
 
-  function handleNameValidation(e){
+  function handleNameValidation(e) {
     let newText = e.target.value;
     setName(newText);
 
     nameValidation(newText);
   }
 
-  function handleFormValidation(e){
+  function handleFormValidation(e) {
     let newText = e.target.value;
     setText(newText);
 
     emailValidation(newText);
-  }  
+  }
 
-  function emailValidation(input){
-    if(emailVerification){
-      switch(true){
+  function emailValidation(input) {
+    if (emailVerification) {
+      switch (true) {
         case input.trim().length === 0:
-          setEmailError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>O campo não pode estar em branco</p></div>);
+          setEmailError(
+            <div className="errorMsg">
+              <img src="/static/images/x.webp" />
+              <p>O campo não pode estar em branco</p>
+            </div>
+          );
           break;
         case /\s/.test(input):
-          setEmailError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>Não pode conter espaços</p></div>);
+          setEmailError(
+            <div className="errorMsg">
+              <img src="/static/images/x.webp" />
+              <p>Não pode conter espaços</p>
+            </div>
+          );
           break;
         case !/@/.test(input):
-          setEmailError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>Precisa conter @</p></div>);
+          setEmailError(
+            <div className="errorMsg">
+              <img src="/static/images/x.webp" />
+              <p>Precisa conter @</p>
+            </div>
+          );
           break;
-          case !/\./.test(input):
-            setEmailError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>Precisa conter .com ou semelhantes</p></div>);
-            break;
+        case !/\./.test(input):
+          setEmailError(
+            <div className="errorMsg">
+              <img src="/static/images/x.webp" />
+              <p>Precisa conter .com ou semelhantes</p>
+            </div>
+          );
+          break;
         default:
           setEmailError(null);
-          break;      
+          break;
       }
-    } else{
-    setEmailError("");
+    } else {
+      setEmailError("");
     }
   }
 
-  function nameValidation(input){
-    if(nameVerificarion && input.trim().length === 0){
+  function nameValidation(input) {
+    if (nameVerificarion && input.trim().length === 0) {
       console.log("Error no nome");
-      setNameError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>O campo não pode estar em branco</p></div>)
-    } else{
+      setNameError(
+        <div className="errorMsg">
+          <img src="/static/images/x.webp" />
+          <p>O campo não pode estar em branco</p>
+        </div>
+      );
+    } else {
       setNameError("");
     }
   }
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     nameValidation(name);
-        
-    if(name.trim().length === 0){
-        setNameError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>Preencha o campo novamente</p></div>);
-    } else{
-        submitForm();
+
+    if (name.trim().length === 0) {
+      setNameError(
+        <div className="errorMsg">
+          <img src="/static/images/x.webp" />
+          <p>Preencha o campo novamente</p>
+        </div>
+      );
+    } else {
+      submitForm();
     }
   }
 
-  function submitForm(){
-    if(!isSubmitting){
+  function submitForm() {
+    if (!isSubmitting) {
       setIsSubmitting(true);
       emailValidation(text);
-      if(!emailError && text.trim().length > 0){
-        console.log('Enviando...');
+      if (!emailError && text.trim().length > 0) {
+        console.log("Enviando...");
         setIsLoading(true);
 
         setTimeout(() => {
@@ -90,49 +120,89 @@ function Form(){
           setIsLoading(false);
           setIsSend(true);
         }, 3500);
-
-      } else{
+      } else {
         console.log("Error");
-        setEmailError(<div className='errorMsg'><img src='/src/images/x.webp'/><p>Preencha o campo corretamente</p></div>);
+        setEmailError(
+          <div className="errorMsg">
+            <img src="/static/images/x.webp" />
+            <p>Preencha o campo corretamente</p>
+          </div>
+        );
         setIsSubmitting(false);
         setIsLoading(false);
       }
     }
   }
 
-  return(
+  return (
     <>
-    <form>
-      <h2>Register</h2>
-      <div>
-        <label htmlFor="name" aria-hidden="true">Name</label>
-        <input placeholder="Name" id="name" onChange={handleNameValidation} value={name} autoComplete='off' 
-          className={nameError ? 'input-error' : 'input'} onFocus={() => {setNameVerification(true)}}
-          onBlur={() => {setNameVerification(false)}} type='text'/>
-        <p className='error'>{nameVerificarion && nameError}</p>
+      <form>
+        <h2>Register</h2>
+        <div>
+          <label htmlFor="name" aria-hidden="true">
+            Name
+          </label>
+          <input
+            placeholder="Name"
+            id="name"
+            onChange={handleNameValidation}
+            value={name}
+            autoComplete="off"
+            className={nameError ? "input-error" : "input"}
+            onFocus={() => {
+              setNameVerification(true);
+            }}
+            onBlur={() => {
+              setNameVerification(false);
+            }}
+            type="text"
+          />
+          <p className="error">{nameVerificarion && nameError}</p>
+        </div>
+        <div>
+          <label htmlFor="e-mail" aria-hidden="true" autoComplete="off">
+            E-mail
+          </label>
+          <input
+            placeholder="E-mail"
+            id="e-mail"
+            value={text}
+            onChange={handleFormValidation}
+            className={emailError ? "input-error" : "input"}
+            autoComplete="off"
+            onFocus={() => {
+              setEmailVerification(true);
+            }}
+            onBlur={() => {
+              setEmailVerification(false);
+            }}
+            type="text"
+          />
+          <p className="error">{emailVerification && emailError}</p>
+        </div>
+        <div className="button">
+          <button onClick={handleSubmit} type="submit">
+            Submit
+          </button>
+          <img
+            style={{ display: isLoading ? "flex" : "none" }}
+            src="/static//images/loading.gif"
+            alt="loading..."
+          />
+        </div>
+      </form>
+      <div className={`modal ${isSend ? "visible" : ""}`}>
+        <div className="fade-in">
+          <img src={reactLogo} alt="React logo" />
+          <h1>God Job {name}</h1>
+          <p>you will receive an email to {text}</p>
+          <p style={{ color: "#ffffff9a", fontSize: "0.8em" }}>
+            (no email will be sent to {text} this is just a test)
+          </p>
+        </div>
       </div>
-      <div>
-        <label htmlFor="e-mail" aria-hidden="true" autoComplete='off'>E-mail</label>
-        <input placeholder="E-mail" id="e-mail"
-          value={text} onChange={handleFormValidation}  className={emailError ? 'input-error' : 'input'} autoComplete='off'
-          onFocus={() => {setEmailVerification(true)}} onBlur={() => {setEmailVerification(false)}} type='text'/>
-        <p className='error'>{emailVerification && emailError}</p>
-      </div>
-      <div className='button'>
-      <button onClick={handleSubmit} type="submit">Submit</button>
-        <img style={{display: isLoading ? 'flex' : 'none'}} src='/src/images/loading.gif' alt='loading...'/>
-      </div>
-    </form>
-    <div className={`modal ${isSend ? 'visible' : ''}`}>
-      <div className='fade-in'>
-        <img src={reactLogo} alt='React logo'/>
-        <h1>God Job {name}</h1>
-        <p>you will receive an email to {text}</p>
-        <p style={{color: '#ffffff9a', fontSize: '0.8em'}}>(no email will be sent to {text} this is just a test)</p>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
 function App() {
@@ -141,7 +211,7 @@ function App() {
       <Form></Form>
       <Text></Text>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Text.jsx
+++ src/Text.jsx
@@ -1,15 +1,21 @@
-import './styles/App.css'
+import "./styles/App.css";
 
-import reactLogo from './images/img.webp';
+import reactLogo from "/static/images/img.webp";
 
-function Text(){
-    return(
-        <aside>
-            <img src={reactLogo} alt='React logo'/>
-            <h1>React Form</h1>
-            <p>A react form made to test the self-validation function. The entire code can be seen at the <a href='' target='_blank'>Link</a></p>
-        </aside>
-    )
+function Text() {
+  return (
+    <aside>
+      <img src={reactLogo} alt="React logo" />
+      <h1>React Form</h1>
+      <p>
+        A react form made to test the self-validation function. The entire code
+        can be seen at the{" "}
+        <a href="" target="_blank">
+          Link
+        </a>
+      </p>
+    </aside>
+  );
 }
 
-export default Text
+export default Text;
